4413180de3fba32910baa98a8f2203b79e950f99
Igor Lebovic <igor.lebovic@gmail.com>
2012-01-15 16:22:23 -0500

    added helper and stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def title
-    base_title = "RoR Tutorial Sample App"
+    base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
       base_title
     else
